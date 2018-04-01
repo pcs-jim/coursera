6a8a064c48c3e006d7be38d5237dda9f88a6df33
porkchop-jim <cstarr114@gmail.com>
2018-04-01 22:37:43 +0900

    n queens - brute force & backtracking examples added

---
+++ proofs/n_queens.py
@@ -0,0 +1,51 @@
+import itertools as it
+
+'''
+This is my solution for the n queens problem on Coursera course for 'What is a proof?'
+'''
+
+
+# Brute force solution
+def is_solution(perm):
+    for (i1, i2) in it.combinations(range(len(perm)), 2):
+        if abs(i1 - i2) == abs(perm[i1] - perm[i2]):
+            return False
+
+    return True
+
+
+for brute_perm in it.permutations(range(10)):
+    if is_solution(brute_perm):
+        print(brute_perm)
+        break
+
+
+# Backtracking solution
+def can_be_extended_to_solution(perm):
+    i = len(perm) - 1
+    for j in range(i):
+        if i - j == abs(perm[i] - perm[j]):
+            return False
+    return True
+
+
+def extend(perm, n):
+    if len(perm) == n:
+        print(perm)
+        exit()
+
+    for k in range(n):
+        if k not in perm:
+            perm.append(k)
+
+            if can_be_extended_to_solution(perm):
+                extend(perm, n)
+
+            perm.pop()
+
+
+extend([], 10)
+
+
+
+
